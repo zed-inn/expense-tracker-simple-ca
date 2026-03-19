15d878b7e035902cd1f3aeb9772fa46f32273694
Zed <skhull.coding@gmail.com>
2026-03-19 18:23:43 +0530

    feat: update expense

---
+++ core/src/application/usecases/update-expense.usecase.ts
@@ -0,0 +1,35 @@
+import { DomainError } from "@error/domain.error";
+import { ExpenseRepository } from "@interface/expense-repo.interface";
+
+type UpdateExpenseParameters = {
+  id: string;
+  description?: string;
+  amount?: number;
+};
+
+export class UpdateExpense {
+  constructor(private expenseRepo: ExpenseRepository) {}
+
+  async execute(params: UpdateExpenseParameters) {
+    if (!params.description && !params.amount)
+      throw new UpdateExpenseParametersInsufficientError();
+
+    const expense = await this.expenseRepo.getById(params.id);
+
+    if (params.description) expense.redescribe(params.description);
+    if (params.amount) expense.changeAmount(params.amount);
+
+    await this.expenseRepo.save(expense);
+  }
+}
+
+export class UpdateExpenseParametersInsufficientError extends DomainError {
+  constructor() {
+    super({
+      error: "invalid_action",
+      code: "PARAMETERS_INSUFFICIENT",
+      message:
+        "Either description or amount or both are required to update an expense.",
+    });
+  }
+}
